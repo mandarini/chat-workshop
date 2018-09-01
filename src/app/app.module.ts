0f88926c2fff68ca54196336dce6a4f1d1c4ad0d
katerina <sk.katherine@gmail.com>
2018-08-31 23:26:58 -0500

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { MessageComponent } from './message/message.component';
+import { ChatComponent } from './chat/chat.component';
+import { InputComponent } from './input/input.component';
+
+const config = {
+    apiKey: "AIzaSyCq_8sc1_OeUBViQkmazhoyL97HqMtmrDI",
+    authDomain: "chat-psybercity.firebaseapp.com",
+    databaseURL: "https://chat-psybercity.firebaseio.com",
+    projectId: "chat-psybercity",
+    storageBucket: "chat-psybercity.appspot.com",
+    messagingSenderId: "419036519980"
+};
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    MessageComponent,
+    ChatComponent,
+    InputComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
